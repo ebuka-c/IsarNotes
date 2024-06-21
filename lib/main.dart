eda3cc920142582c5f5cc6dfacd463f6242b067b
ebuka-c <ebukaachi10@gmail.com>
2024-06-21 23:22:48 +0100

    changed app desc

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Notes with Isar DB',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
